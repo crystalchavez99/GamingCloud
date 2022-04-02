65a08ed84b79204f970dad9f531c2e5d099c491e
Crystal <crystal120599@gmail.com>
2022-04-01 19:40:51 -0700

    redux prov and router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
 import './index.css';
+
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 
 ReactDOM.render(
   <React.StrictMode>
