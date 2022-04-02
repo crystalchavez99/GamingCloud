adc3b21341c64cf090022887f3e3f242d6f431f2
Crystal <crystal120599@gmail.com>
2022-04-01 19:21:54 -0700

    setup redux store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,6 @@
+// frontend/src/store/index.js
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+});
