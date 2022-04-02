8e90ca77cca1549f7c5478fe33421fab3c3e480e
Crystal <crystal120599@gmail.com>
2022-04-01 18:37:07 -0700

    security middleware

--- backend/app.js
+++ backend/app.js
@@ -10,3 +10,27 @@ const isProduction = environment === 'production';
 
 const app = express();
 app.use(morgan('dev'));
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+  }
+
+  // helmet helps set a variety of headers to better secure your app
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+  // Set the _csrf token and create req.csrfToken method
+  app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
