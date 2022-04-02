376ac34a54afdbb48f49159dcccbeebfde279934
Crystal <crystal120599@gmail.com>
2022-04-01 18:38:24 -0700

    app route setup

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -34,3 +35,5 @@ if (!isProduction) {
       }
     })
   );
+
+app.use(routes);
