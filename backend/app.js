afb10c186d3c9d763a4167ee532c49c817a721b0
Crystal <crystal120599@gmail.com>
2022-04-01 18:44:51 -0700

    sequelize error

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -46,6 +47,16 @@ app.use((_req, _res, next) => {
   err.title = "Resource Not Found";
   err.errors = ["The requested resource couldn't be found."];
   err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
   next(err);
 });
 module.exports = app;
