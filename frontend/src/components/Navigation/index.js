0be786dd8e9542bb8b13469a9717d6fc628ec0ed
Crystal <crystal120599@gmail.com>
2022-04-04 10:43:17 -0700

    upload

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -11,7 +11,10 @@ function Navigation({ isLoaded }) {
     let sessionLinks;
     if (sessionUser) {
         sessionLinks = (
+            <>
             <ProfileButton user={sessionUser} />
+            <NavLink to="/upload">Upload</NavLink>
+            </>
         );
     } else {
         sessionLinks = (
