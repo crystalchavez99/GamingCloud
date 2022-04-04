2ba004d1dd9baf17685167e43d4e9e478a323229
Crystal <crystal120599@gmail.com>
2022-04-04 08:22:57 -0700

    banner looks good for start

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -3,6 +3,7 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect , useHistory} from 'react-router-dom';
 import './SplashPage.css';
+import banner from '../../images/banner.jpg';
 
 function SplashPage() {
     return(
@@ -11,7 +12,9 @@ function SplashPage() {
                 <h1><span>GamingCloud</span></h1>
                 <p>GamingCloud gives our users the ability to create your musicial talent, find fans & industries, and connect with others. </p>
             </div>
-
+            <div>
+                <p>[WIP] Will Implement List of Songs and Artists</p>
+            </div>
         </div>
     )
 }
