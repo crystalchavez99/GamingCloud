8b83d8e7f9e5c402999b4905a172df731237bc01
Crystal <crystal120599@gmail.com>
2022-04-04 13:56:18 -0700

    get song id and all songs work except post right now

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,17 +3,32 @@ const express = require('express');
 const db = require('../../db/models');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
+const {requireAuth} = require("../../utils/auth");
+const csrf = require("csurf");
 
+const csrfProtection = csrf({ cookie: true });
 
  router.get('/',asyncHandler(async(req,res)=>{
      const songs = await db.Song.findAll();
      console.log(songs);
      return res.json({songs});
  }));
-// router.get(`/:songId`,asyncHandler(async(req,res)=>{
-//     const id = parseInt(req.params.songId);
-//     const song = await db.Song.findByPk(id);
-//     return res.json(song)
-// }))
+ router.post(`/`,requireAuth,asyncHandler(async (req,res)=>{
+     const {title,url,genre} = req.body;
+     const {userId} = req.user.id;
+     const song = await db.Song.create({
+         title,
+         url,
+         genre,
+         userId,
+         csrfToken: req.csrfToken(),
+     })
+     return res.json({song})
+ }))
+ router.get(`/:songId`,asyncHandler(async(req,res)=>{
+     const id = parseInt(req.params.songId);
+     const song = await db.Song.findByPk(id);
+     return res.json(song)
+ }))
 
 module.exports =router;
