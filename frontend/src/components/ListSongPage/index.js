acc42afec808bcbef10348e0b4cade6192c43770
Crystal <crystal120599@gmail.com>
2022-04-04 15:31:13 -0700

    State to load songs works

--- frontend/src/components/ListSongPage/index.js
+++ frontend/src/components/ListSongPage/index.js
@@ -1,21 +1,14 @@
 import useParams from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { loadASong } from '../../store/song';
+import {getAllSongs} from '../../store/song';
 import './ListSongPage.css';
 function ListSongPage(){
-    // const {songId} = useParams();
-    // const dispatch = useDispatch();
-    // const song = useSelector(state => state.songs[songId]);
-
-    // useEffect(()=>{
-    //     dispatch(loadASong(songId))
-    // },[dispatch,songId])
-    // return(
-    //     <div>
-    //         <h2>{song.title}</h2>
-    //     </div>
-    // )
+    const dispatch = useDispatch();
+    const songs = useSelector((state)=> Object.values(state.song));
+    useEffect(()=>{
+        dispatch(getAllSongs());
+    },[dispatch])
     return(
         <div>
             <h1>SONG LIST</h1>
