4449e99ce80756a7b32555739fe335697a458c02
Crystal <crystal120599@gmail.com>
2022-04-03 21:09:50 -0700

    start splash

---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect , useHistory} from 'react-router-dom';
+
+function SplashPage() {
+
+}
+
+export default SplashPage;
