942ea97b88b0c2765455de6d6563e754cb29c6bb
Crystal <crystal120599@gmail.com>
2022-04-03 21:25:18 -0700

    set up land page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect , useHistory} from 'react-router-dom';
+import './SplashPage.css';
 
 function SplashPage() {
-
+    return(
+        <div className='splash'>
+            <h1>GamingCloud</h1>
+        </div>
+    )
 }
 
 export default SplashPage;
