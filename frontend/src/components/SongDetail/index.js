2f80e8f6f0d343b671b1353aa5add4fd67887b5b
Crystal <crystal120599@gmail.com>
2022-04-04 21:47:21 -0700

    work in progress

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -1,8 +1,9 @@
 const SongDetail = ({id,userId,title,url,genre})=>{
     return(
         <div className="song-detail">
-            <h1>{title}</h1>
-            <h3>{genre}</h3>
+            {/* <h1>{title}</h1>
+            <h3>{genre}</h3> */}
+            {/*WORK IN PROGRESS */}
         </div>
     )
 }
