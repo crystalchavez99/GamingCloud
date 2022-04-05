447d51ebd6b9d941b40b0c221fbeb3609f3c14a6
Crystal <crystal120599@gmail.com>
2022-04-04 22:38:20 -0700

    done for the day

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -1,9 +1,22 @@
-const SongDetail = ({id,userId,title,url,genre})=>{
-    return(
-        <div className="song-detail">
-            <h1>{title}</h1>
-            <h3>{genre}</h3>
-        </div>
-    )
-}
-export default SongDetail;
+// import { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useParams } from 'react-router-dom';
+// import { editSong } from '../store/song';
+// const SongDetail = ()=>{
+//     // const dispatch = useDispatch();
+//     // const {songId} = useParams();
+//     // const songg = useSelector(state => Object.values(state.song))
+//     // const [editTitle, seteEditTitle] = useState(song.title);
+//     // const [editGenre, setEditGenre] = useState(song.genre);
+//     // const [editUrl, seteEditUrl] = useState(song.url);
+//     // useEffect(()=>{
+//     //     dispatch(editSong(songId))
+//     // })
+//     // return(
+//     //     <div className="song-detail">
+//     //         <h1>{song.title}</h1>
+//     //         <h3>{song.genre}</h3>
+//     //     </div>
+//     // )
+// }
+// // export default SongDetail;
--- frontend/src/components/UploadPage/index.js
+++ frontend/src/components/UploadPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './UploadPage.css';
 import { addSong } from '../../store/song';
 
