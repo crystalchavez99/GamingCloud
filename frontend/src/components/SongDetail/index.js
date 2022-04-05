fc9a81fb521f12ce8fb4922808aef2917ce4a5f9
Crystal <crystal120599@gmail.com>
2022-04-05 09:10:29 -0700

    song detail renders correct song

--- frontend/src/components/SongDetail/index.js
+++ frontend/src/components/SongDetail/index.js
@@ -1,18 +1,30 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { getAllSongs, getSong } from "../../store/song";
 
 function SongDetail(){
     const sessionUser = useSelector(state => state.session.user);
     const {songId} = useParams();
+    // console.log(songId)
     const dispatch = useDispatch();
-    const song = useSelector(state => state.song);
-    console.log(sessionUser);
+    const songs = useSelector((state) => state.song);
+    console.log(songs[songId])
+    const song = songs[songId];
     console.log(song)
+    useEffect(() =>  {
+        dispatch(getAllSongs());
+    }, [dispatch])
+    if (!songs) {
+        return null;
+    }
+    //const soloSong = songs.find((song,index)=> song.id === songId)
+    //console.log(soloSong)
     return(
-        <>
-        <h1>Song Title</h1>
-        </>
+        <div className={`songdetail ${song.id}`}>
+        <h1>{song.title}</h1>
+
+        </div>
     )
 }
 
