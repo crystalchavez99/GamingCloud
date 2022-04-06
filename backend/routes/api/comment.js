7ba1a2dd242a902918e5eca68ca426e0ad5bd78e
Crystal <crystal120599@gmail.com>
2022-04-06 08:59:44 -0700

    start route

--- backend/routes/api/comment.js
+++ backend/routes/api/comment.js
@@ -3,3 +3,11 @@ const db = require('../../db/models');
 const asyncHandler = require('express-async-handler');
 const router = express.Router();
 const {requireAuth} = require("../../utils/auth");
+
+router.get('/',asyncHandler(async(req,res)=>{{
+    const comments = await db.Comment.findAll({
+        order: [["createdAt", "DESC"]]
+    });
+}}));
+
+
