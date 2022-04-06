4e6d1be841f5e8d276d31f663b38010ce2e0375a
Crystal <crystal120599@gmail.com>
2022-04-06 08:30:56 -0700

    seeders set up

---
+++ backend/db/seeders/20220406152809-commentSeed.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Comments', [
+      {
+        userId: 2,
+        songId: 2,
+        body: 'This is my favorite Pokemon OST!'
+      },
+      {
+        userId: 3,
+        songId: 1,
+        body: 'POG!'
+      },
+    ], {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Comments', null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
