f278f26d7c4d84bf32c7ef5a5ecae8de3c5c2700
Crystal <crystal120599@gmail.com>
2022-04-06 16:28:30 -0700

    validators

--- frontend/src/components/UploadPage/index.js
+++ frontend/src/components/UploadPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './UploadPage.css';
@@ -11,9 +11,26 @@ function UploadPage({ user }) {
     const [genre, setGenre] = useState('');
     const [url, setUrl] = useState('');
     const [songCover, setSongCover] = useState('');
+    const [errors, setErrors] = useState([]);
     const history = useHistory();
     const dispatch = useDispatch();
-
+    useEffect(() => {
+        const errors = [];
+        if (!title) {
+            errors.push('Please provide a title!')
+        }
+        if (!genre) {
+            errors.push('Please provide a genre!')
+        }
+        if (!url) {
+            errors.push('Please provide a url!')
+        }
+        if (!songCover) {
+            errors.push('Please provide a songCover!')
+        }
+        setErrors(errors)
+        //console.log(errors)
+    }, [title, genre, url, songCover]);
     if (!sessionUser) {
         return null;
     }
@@ -28,41 +45,57 @@ function UploadPage({ user }) {
             userId: sessionUser.id
         }
         await dispatch(addSong(payload));
+        setErrors([]);
         history.push("/songs");
     }
+
     return (
         <div>
             <h1>Upload</h1>
             <p>Take your gaming music to the next level.</p>
             <div className='upload'>
                 <form onSubmit={handleSubmit}>
+                    {errors.length > 0 && (
+                        <div className='errors'>
+                            The following errors were found:
+                            <ul>
+                                {errors.map(error => (
+                                    <li key={error}>{error}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
                     <label>
                         Title
                         <input
                             onChange={e => setTitle(e.target.value)}
                             value={title}
-                            placeholder="Title" />
+                            placeholder="Title"
+                             />
                     </label>
                     <label>
                         Genre
                         <input
                             onChange={e => setGenre(e.target.value)}
                             value={genre}
-                            placeholder="Genre" />
+                            placeholder="Genre"
+                             />
                     </label>
                     <label>
                         Song Cover
                         <input
                             onChange={e => setSongCover(e.target.value)}
                             value={songCover}
-                            placeholder="Song Cover" />
+                            placeholder="Song Cover"
+                             />
                     </label>
                     <label>
                         Url
                         <input
                             onChange={e => setUrl(e.target.value)}
                             value={url}
-                            placeholder="Url" />
+                            placeholder="Url"
+                             />
                     </label>
                     <button className='uploadbutton' type='submit'>Upload</button>
                 </form>
